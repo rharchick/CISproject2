228d0d5b6ef30042a3cf50378e16e4dea6a62d30
rharchick <rharchick@gmail.com>
2015-03-18 13:09:38 -0400

    Create proj2.c

---
+++ proj2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+
+int main(){
+	
+	//top-level logic
+	// handels user environment / commands
+	
+}
+
+void fmount(){
+	
+}
+
+void fumount(){
+	
+}
+
+void structure(){
+	
+}
+
+void traverse(){
+	
+}
+
+void showsector(){
+	
+}
+
+void showfat(){
+	
+}
+
+// more functions to add...
