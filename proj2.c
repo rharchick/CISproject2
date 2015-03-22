b163858e94a48a6f4a356a63acdfc9920db65aed
Samuel Turnage <samturnage4@yahoo.com>
2015-03-22 00:36:14 -0400

    Update proj2.c

--- proj2.c
+++ proj2.c
@@ -140,7 +140,7 @@ void PrintMoreSectorInfo(unsigned int fileInfoDec[], char fileInfo[])
     dayNum = (((monthDay/16)*10) + (monthDay%16)); //convert dayNum to hex
     if (dayNum > 32) //day must be 31 or less, if it is not already then the remainder is the correct day
         dayNum = dayNum % 32;
-    printf("%d/%d/%d \t\t", monthNum, dayNum, year); //print date
+    printf("%d/%d/%d \t", monthNum, dayNum, year); //print date
     //TIME
     unsigned int hourMin = fileInfoDec[15]; //upper byte used for hour/minute calculation
     unsigned int minSec = fileInfoDec[14]; //lower byte used for minute/second calculation
@@ -154,7 +154,7 @@ void PrintMoreSectorInfo(unsigned int fileInfoDec[], char fileInfo[])
     //SECONDS
     minSec = fileInfoDec[14]; //restore minSec
     minSec = (minSec%32); //cast off upper 3 bits to obtain seconds
-    printf("%02d \t\t", minSec);
+    printf("%02d \t", minSec);
     
     //File Size
     unsigned int newDecValue = 0; //used to store return value of multiByteHexToDec
@@ -180,7 +180,7 @@ void traverse(short flag)
         printf("** H ------ HIDDEN FILE    **\n");
         printf("** A ------ ARCHIVE FILE   **\n");
         printf("*****************************\n");
-        printf("ATTRIB\tDATE\tTIME\tSIZE\tSECTOR\tNAME");
+        printf("ATTRIB\t\tDATE\t\tTIME\tSIZE\tSECTOR\tNAME");
     }
     
     for (sector = 19; sector < 33; ++sector) //reads through sectors 19-32 (root sectors)
